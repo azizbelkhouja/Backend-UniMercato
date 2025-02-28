518471aba2505903e3eb7d68a5de3c28e80dc3ab
Mohamed Aziz Belkhouja <belkhoujamohamedaziz@gmail.com>
2025-02-28 16:31:21 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# UniMercato
+ Una piattaforma di e-commerce multivendor per studenti universitari, progettata per comprare e vendere oggetti usati in modo facile e sicuro.
---
+++ backend/src/main/java/com/aziz/UnimercatoApplication.java
@@ -0,0 +1,13 @@
+package com.aziz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnimercatoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnimercatoApplication.class, args);
+	}
+
+}
