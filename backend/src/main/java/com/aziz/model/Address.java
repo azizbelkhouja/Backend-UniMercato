29f3cf904ae9569f39b1ef5e8e4f03d4cfdb3a97
Mohamed Aziz Belkhouja <belkhoujamohamedaziz@gmail.com>
2025-03-02 17:46:38 +0100

    Address, Coupon Entities

--- backend/src/main/java/com/aziz/model/Address.java
+++ backend/src/main/java/com/aziz/model/Address.java
@@ -1,4 +1,34 @@
 package com.aziz.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
 public class Address {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO) // generate Id automatically ( no id from frontend when new user created )
+    private Long id;
+
+    private String name;
+
+    private String locality;
+
+    private String address;
+
+    private String city;
+
+    private String province;
+
+    private String postalCode;
+
+    private String mobile;
 }
--- backend/src/main/java/com/aziz/model/Coupon.java
+++ backend/src/main/java/com/aziz/model/Coupon.java
@@ -1,4 +1,22 @@
 package com.aziz.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
 public class Coupon {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO) // generate Id automatically ( no id from frontend when new user created )
+    private Long id;
+
+
 }
