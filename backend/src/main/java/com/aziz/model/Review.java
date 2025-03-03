b66e8587646eb1dd3dbc4454df53b2914d5eb676
Mohamed Aziz Belkhouja <belkhoujamohamedaziz@gmail.com>
2025-03-03 15:59:21 +0100

    Review Entity
    - Many-to-One with Product: A review is for one product.
    - Many-to-One with User: A review is written by one user.

--- backend/src/main/java/com/aziz/model/Review.java
+++ backend/src/main/java/com/aziz/model/Review.java
@@ -1,4 +1,42 @@
 package com.aziz.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
 public class Review {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String reviewText;
+
+    private double rating;
+
+    @ElementCollection
+    private List<String> productImages;
+
+    @JsonIgnore
+    @ManyToOne
+    @NotNull
+    private Product product;
+
+    @ManyToOne
+    @NotNull
+    private User user;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt= LocalDateTime.now();
+
 }
