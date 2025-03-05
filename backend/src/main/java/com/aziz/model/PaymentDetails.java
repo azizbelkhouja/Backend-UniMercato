0a599b2e3c72d9ee9c8529fe4aeea4b8068eb8c8
Mohamed Aziz Belkhouja <belkhoujamohamedaziz@gmail.com>
2025-03-05 16:52:07 +0100

    OrderItem, PaymentDetails
    + PaymentStatus Enum

---
+++ backend/src/main/java/com/aziz/model/PaymentDetails.java
@@ -0,0 +1,15 @@
+package com.aziz.model;
+
+import com.aziz.domain.PaymentStatus;
+import lombok.Data;
+
+@Data
+public class paymentDetails {
+
+    private String paymentId;
+    private String stripePaymentLinkId;
+    private String stripePaymentLinkReferenceId;
+    private String stripePaymentLinkStatus;
+    private String stripePaymentIdZWSP;
+    private PaymentStatus status;
+}
