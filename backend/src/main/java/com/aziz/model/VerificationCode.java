7e6eab9fafa46d88d2b868aa2d54a541bfbef682
Mohamed Aziz Belkhouja <belkhoujamohamedaziz@gmail.com>
2025-03-05 17:10:51 +0100

    VerificationCode Entity
    - One-to-One with **User**: A verification code can be associated with one user.
    - One-to-One with **Seller**: A verification code can be associated with one seller.
    16. **SellerReport**

--- backend/src/main/java/com/aziz/model/VerificationCode.java
+++ backend/src/main/java/com/aziz/model/VerificationCode.java
@@ -3,29 +3,25 @@ package com.aziz.model;
 import jakarta.persistence.*;
 import lombok.*;
 
-import java.time.LocalDateTime;
-
 @Entity
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @EqualsAndHashCode
-public class Transaction {
+public class VerificationCode {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
-    @ManyToOne
-    private User customer;
+    private String otp;
+
+    private String email;
 
     @OneToOne
-    private Order order;
+    private User user;
 
-    @ManyToOne
+    @OneToOne
     private Seller seller;
-
-    //when transaction is created
-    private LocalDateTime date = LocalDateTime.now();
 }
