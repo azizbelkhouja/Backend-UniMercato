43db94573efa39d6d7a198ead62ea90c64fe49fb
Mohamed Aziz Belkhouja <belkhoujamohamedaziz@gmail.com>
2025-03-06 15:30:54 +0100

    feat: Implement JWT token validation in security filter chain

--- backend/src/main/java/com/aziz/config/AppConfig.java
+++ backend/src/main/java/com/aziz/config/AppConfig.java
@@ -1,4 +1,27 @@
 package com.aziz.config;
 
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
 public class AppConfig {
+
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.sessionManagement(management -> management.sessionCreationPolicy(
+                SessionCreationPolicy.STATELESS
+        )).authorizeRequests(authorize -> authorize
+                .requestMatchers("/api/**").authenticated()
+                .requestMatchers("/api/products/*/reviews").permitAll()
+                .anyRequest().permitAll()
+        ).addFilterBefore(new JwtTokenValidator(), BasicAuthenticationFilter.class);
+
+        return null;
+    }
+
 }
