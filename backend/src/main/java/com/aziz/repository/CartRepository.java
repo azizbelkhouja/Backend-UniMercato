4e5debc34614e6be025c94c58a88576e33b28681
Mohamed Aziz Belkhouja <belkhoujamohamedaziz@gmail.com>
2025-03-06 16:58:56 +0100

    AuthResponse

---
+++ backend/src/main/java/com/aziz/repository/CartRepository.java
@@ -0,0 +1,10 @@
+package com.aziz.repository;
+
+import com.aziz.modal.Cart;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CartRepository extends JpaRepository<Cart, Long> {
+
+    Cart findByUserId(Long id);
+
+}
--- backend/src/main/java/com/aziz/service/AuthService.java
+++ backend/src/main/java/com/aziz/service/AuthService.java
@@ -1,4 +1,16 @@
 package com.aziz.service;
 
-public class AuthService {
+import com.aziz.domain.USER_ROLE;
+import com.aziz.request.LoginRequest;
+import com.aziz.response.AuthResponse;
+import com.aziz.response.SignupRequest;
+
+public interface AuthService {
+
+    void sendOtp(String email, USER_ROLE role) throws Exception;
+
+
+    String createUser(SignupRequest req) throws Exception;
+
+    AuthResponse signin(LoginRequest req) throws Exception;
 }
